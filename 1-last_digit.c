ee658eb14d4737a710baa9e54e26118fddad9afd
Nusroh123 <oyedelenusroh@gmail.com>
2023-03-17 20:36:39 -0700

    Task 1

--- 1-last_digit.c
+++ 1-last_digit.c
@@ -11,15 +11,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n>0)
+	if (n>5)
 	{
-		printf("is positive\n");
+		printf("Last digit of n is \n%d and is greater than 5",n);
 	}
 	else if (n==0)
 	{
-		printf("is zero\n");
+		printf("Last digit of n is \n%d and is 0",n);
 	}
 	else
-		printf("is negative\n");
+		printf("Last digit of n is \n%d and is less than 6 and not 0",n);
 	return (0);
+
 }
