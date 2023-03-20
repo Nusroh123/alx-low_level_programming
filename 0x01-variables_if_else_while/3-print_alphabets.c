7aa065ecabf559dc401c04d2cb2a51a2a95f2119
Nusroh123 <oyedelenusroh@gmail.com>
2023-03-19 21:00:59 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,11 +7,15 @@
  */
 int main(void)
 {
-	char ch;
+	char c, ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
 	}
 	putchar('\n');
 	return (0);
