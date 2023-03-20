d9b9a69b8015a2ef51a7beeb8f1a7f3a65dcf57e
Nusroh123 <oyedelenusroh@gmail.com>
2023-03-19 21:51:35 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Alway 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'q' || c == 'e')
+			putchar('\0');
+		else
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
