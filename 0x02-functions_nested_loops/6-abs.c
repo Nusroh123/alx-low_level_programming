74213870d0d7e32ed7dda31693fd24bd962eace0
Nusroh123 <oyedelenusroh@gmail.com>
2023-03-24 06:28:11 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - calculates the absolute value
+ * @j:  number to be calculated
+ * Return: Always 0
+ */
+int _abs(int j)
+{
+	if (j < 0)
+		j *= -1;
+
+	return (0);
+}
