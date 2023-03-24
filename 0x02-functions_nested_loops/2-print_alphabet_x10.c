6fdedf7b23666492134e5d6ed9f9713155a76e91
Nusroh123 <oyedelenusroh@gmail.com>
2023-03-24 01:47:01 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,9 +2,10 @@
 
 /**
  * main - Entry point
- * function print_alphabet_x10
+ * 
  * Return: Always 0 (Success)
  */
+/* function print_alphabet_x10 */
 void print_alphabet_x10(void)
 {
 	int i = 0;
