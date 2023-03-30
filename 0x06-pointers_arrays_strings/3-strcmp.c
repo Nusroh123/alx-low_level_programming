8401022cf91f90cf3e7dcfba3e5346cfa607ca76
Nusroh123 <oyedelenusroh@gmail.com>
2023-03-29 23:44:16 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare between two string
+ * @s1: first string
+ * @s2: second string
+ * Return: an integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] == s2[i])
+		{
+			return (0);
+		}
+		else if (s1[i] > s2[i])
+		{
+			return (15);
+		}
+		else if (s1[i] < s2[i])
+		{
+			return (-15);
+		}
+	}
+	return (1);
+}
