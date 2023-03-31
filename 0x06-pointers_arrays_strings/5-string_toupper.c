4a206be921cf1404419e3bde089dc1e7795cd918
Nusroh123 <oyedelenusroh@gmail.com>
2023-03-30 21:46:07 -0700

    Task 2

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * string_toupper -  changes all lowercase letters to uppercase
+ * @*: pointer
+ * Return: a character
+ */
+char *string_toupper(char *)
+{
+	int i = 0;
+
+	while (*(ch + i) != '\0')
+	{
+		if (*(ch + i) >= 90 && *(ch + i) <= 122;)
+			*(ch + i) = *(ch + i) - '';
+		i++;
+	}
+	return (ch);
+}
