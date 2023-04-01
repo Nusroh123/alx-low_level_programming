35fd823c05179ecf748f52c3824406e26725ce2a
Nusroh123 <oyedelenusroh@gmail.com>
2023-03-31 17:28:29 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,21 +10,14 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
+	int R;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	i = 0;
+
+	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
 	{
-		if (s1[i] == s2[i])
-		{
-			return (0);
-		}
-		else if (s1[i] > s2[i])
-		{
-			return (15);
-		}
-		else if (s1[i] < s2[i])
-		{
-			return (-15);
-		}
+		i++;
 	}
-	return (1);
+	R = s1[i] - s2[i];
+	return (R);
 }
