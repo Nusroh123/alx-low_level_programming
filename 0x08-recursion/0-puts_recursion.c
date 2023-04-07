2a8768ab9914a56defc763b5cff37322ce8095bf
Nusroh123 <oyedelenusroh@gmail.com>
2023-04-06 20:37:21 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,12 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-	int i;
-	int l = strlen(s);
-
-	for (i = 0; i < l; i++)
+	if (*s != '\0')
 	{
-		_putchar(s[i]);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
