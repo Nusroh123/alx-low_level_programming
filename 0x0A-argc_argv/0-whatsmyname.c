1332fcc5c602bdc027e6649fa906d1494e8686a9
Nusroh123 <oyedelenusroh@gmail.com>
2023-04-08 18:19:13 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main entry
+ * @argc: Argument counter
+ * @argv: Argument vector
+ * Return: 0 (Success)
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
