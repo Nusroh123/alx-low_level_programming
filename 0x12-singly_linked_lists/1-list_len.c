45fe710f0a8682e5beb2e8ef36d8ab9d7ae2acf2
Nusroh123 <oyedelenusroh@gmail.com>
2023-04-27 18:49:37 -0700

    fixing betty

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,14 +3,15 @@
 
 /**
  * list_len - returns the number of elements in a linked list_t list
- * @list_t: list of nodes
  * @h: string
  * Return: number of elements in a linked list
  */
 size_t list_len(const list_t *h)
 {
 	const list_t *temp;
-	unsigned int i = 0;
+	unsigned int i;
+
+	i = 0;
 
 	temp = h;
 	while (temp)
