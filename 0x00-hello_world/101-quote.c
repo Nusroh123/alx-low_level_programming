dc1df60ba97bedf477bb063cace6068718cb5bf2
Nusroh123 <oyedelenusroh@gmail.com>
2023-06-06 09:57:07 -0700

    trying to print the correct one

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
-#include <stdio>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <string.h>
 
 /**
- * Main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - Entry point
+ * Return: 0 on success
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-
-	return(1);
+	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write (STDERR_FILENO, err, strlen(err));
+	return (1);
 }
