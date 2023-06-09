d87d29ebbdd0680e2c98868ac7e9f69ac20f1541
Nusroh123 <oyedelenusroh@gmail.com>
2023-06-09 13:16:21 -0700

    printing all arguments received

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ *@argv: argument vector
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+
+	if (argc > 1)
+	{
+		for (count = 0; count < argc; count++)
+			printf("argv[%d] = %s\n", count, argv[count]);
+	}
+	return (0);
+}
