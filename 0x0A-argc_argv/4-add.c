ce0f9db711e4dd7812d554a4d764329eed85b929
Nusroh123 <oyedelenusroh@gmail.com>
2023-06-09 18:42:59 -0700

    adding many numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int num_count, add, i;
+
+	if (argc > 1)
+	{
+		for (num_count = 1; num_count < argc; num_count++)
+		{
+			for (i = 0; argv[num_count][i] != '\0'; i++)
+			{
+				if (!isdigit(argv[num_count][i]))
+				{	
+					printf("Error\n");
+					return (1);
+				}
+			}
+			add += atoi(argv[num_count]);
+		}
+		printf("%d\n", add);
+		
+	}
+	if (argc <= 1)
+		printf("0\n");
+
+	return (0);
+}
