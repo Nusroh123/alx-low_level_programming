7656ae4a78a1e127eb057b535bf25d4ca0b97522
Nusroh123 <oyedelenusroh@gmail.com>
2023-06-13 18:29:27 -0700

    creating a array using malloc with casting

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of character
+ * and iniiatilizes it with a specific character
+ * @size: size of array
+ * @c: the character to be printed
+ * Return: a pointer to the array or NULL
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array = (char *)malloc(size * sizeof(char));
+	unsigned int i;
+
+	if (size == 0)
+		return (0);
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+
+	return (array);
+}
