2e317a8a05340aca4655eeca2e16bca6b61489c6
Nusroh123 <oyedelenusroh@gmail.com>
2023-08-07 21:27:08 -0700

    Executable and Linkable Formating

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,6 +1,29 @@
 #include "main.h"
 
 /**
+ * display_elf_header - display ELF header
+ * @header: header
+ * Return: Nothing
+ */
+void display_elf_header(const Elf64_Ehdr *header)
+{
+	int i;
+
+	printf("Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++) 
+	{
+		printf("%02x ", header->e_ident[i]);
+	}
+	printf("\nClass:   %u-bit\n", header->e_ident[EI_CLASS] == ELFCLASS64 ? 64 : 32);
+	printf("Data:    %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "Unknown");
+	printf("Version: %d (current)\n", header->e_ident[EI_VERSION]);
+	printf("OS/ABI:  %s\n", header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "Other");
+	printf("ABI Version: %u\n", header->e_ident[EI_ABIVERSION]);
+	printf("Type:    %u\n", header->e_type);
+	printf("Entry point address: 0x%lx\n", (unsigned long)header->e_entry);
+}
+
+/**
  * main - Entry point
  * @ac: argument count
  * @av: argument vector
@@ -9,6 +32,7 @@
 int main(int ac, char *av[])
 {
 	int fd;
+	Elf64_Ehdr elfHeader;
 
 	if (ac != 2)
 	{
@@ -23,19 +47,15 @@ int main(int ac, char *av[])
 		return (1);
 	}
 
-	Elf64_Ehdr elfHeader;
 	if (read(fd,&elfHeader, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
 	{
 		perror("Error reading ELF header");
-		close(fd);
-		return (1);
 	}
 
-	printf("ELF Header Information:\n");
-	printf("  Magic: %02x %02x %02x ...\n", elfHeader.e_ident[0], elfHeader.e_ident[1], elfHeader.e_ident[2]);
-	printf("  Type: %u\n", elfHeader.e_type);
-	printf("  Machine: %u\n", elfHeader.e_machine);
-	printf("  Version: %u\n", elfHeader.e_version);
+	if (memcmp(elfHeader.e_ident, ELFMAG, SELFMAG) != 0)
+		perror("Not an ELF file");
+
+	display_elf_header(&elfHeader);
 
 	close(fd);
 	return (0);
