7014b539a84c31a021165f0b79e5b3c3407b3a55
Nusroh123 <oyedelenusroh@gmail.com>
2023-09-14 17:59:02 -0700

    printing elements of a doublyLL

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print int...
+ * @h: int
+ * Return: count
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count = 0;
+
+	const dlistint_t *current = h;
+	if (current == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		count++;
+	}
+	return (count);
+}
