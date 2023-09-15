cb693cd411036ffe46c77a5c5b1828d209dfb5e1
Nusroh123 <oyedelenusroh@gmail.com>
2023-09-14 19:16:35 -0700

    adding a new node at the beginning

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,7 +8,8 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *newNode = (dlistint_t*)malloc(sizeof(dlistint_t));
+	dlistint_t *newNode = malloc(sizeof(dlistint_t));
+
 	if (newNode == NULL)
 	{
 		return (NULL);
